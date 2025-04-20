8a6bb743ea0fb6ed0c98b055624e24bf5824f075
陶浩南 <taoaaron5@gmail.com>
2025-04-20 21:58:11 +1000

    Userlayout structure test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Userlayout from "./components/Layout/Userlayout";
+
 /*
  * @Date: 2025-04-20 21:08:35
  * @LastEditors: 陶浩南 taoaaron5@gmail.com
- * @LastEditTime: 2025-04-20 21:38:31
+ * @LastEditTime: 2025-04-20 21:56:28
  * @FilePath: /E-Commerce-Rabbit/frontend/src/App.jsx
  */
 export default function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-blue-100">
-      <h1 className="text-4xl font-bold text-blue-600">
-        Hello Tailwind + Vite!
-      </h1>
-    </div>
+    <>
+      {/* react-router-dom */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Userlayout />}>
+            {/* User Layout */}
+          </Route>
+          <Route>{/* Admin Layout */}</Route>
+        </Routes> 
+      </BrowserRouter>
+    </>
   );
 }
---
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -0,0 +1,13 @@
+/*
+ * @Date: 2025-04-20 21:54:41
+ * @LastEditors: 陶浩南 taoaaron5@gmail.com
+ * @LastEditTime: 2025-04-20 21:57:17
+ * @FilePath: /E-Commerce-Rabbit/frontend/src/components/Layout/Userlayout.jsx
+ */
+import React from "react";
+
+const Userlayout = () => {
+  return <div className="text-blue-500">Userlayout</div>;
+};
+
+export default Userlayout;
