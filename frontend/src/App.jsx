eb80086db5faf242677f2fc71e9bcadb6846c586
陶浩南 <taoaaron5@gmail.com>
2025-04-20 21:38:58 +1000

    tailwindcss set up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+/*
+ * @Date: 2025-04-20 21:08:35
+ * @LastEditors: 陶浩南 taoaaron5@gmail.com
+ * @LastEditTime: 2025-04-20 21:38:31
+ * @FilePath: /E-Commerce-Rabbit/frontend/src/App.jsx
+ */
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex items-center justify-center h-screen bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Hello Tailwind + Vite!
+      </h1>
+    </div>
+  );
 }
-
-export default App
