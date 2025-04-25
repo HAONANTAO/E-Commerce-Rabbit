2a92912f3e166bb3109eaca7198be5d5cdd9a51a
陶浩南 <taoaaron5@gmail.com>
2025-04-25 18:55:49 +1000

    quantity add and remove

--- frontend/src/components/Cart/CartContent.jsx
+++ frontend/src/components/Cart/CartContent.jsx
@@ -1,7 +1,7 @@
 /*
  * @Date: 2025-04-25 18:31:40
  * @LastEditors: 陶浩南 taoaaron5@gmail.com
- * @LastEditTime: 2025-04-25 18:40:54
+ * @LastEditTime: 2025-04-25 18:54:56
  * @FilePath: /E-Commerce-Rabbit/frontend/src/components/Cart/CartContent.jsx
  */
 import React from "react";
@@ -37,7 +37,28 @@ const CartContent = () => {
             key={index}
             className="flex justify-between items-start py-4 border-b">
             <div className="flex items-start">
-              <img src={product.image} alt={product.name} />
+              <img
+                src={product.image}
+                alt={product.name}
+                className="object-cover w-20 h-24 rounded"
+              />
+              <div className="ml-4">
+                <h3>{product.name}</h3>
+                <p className="text-sm text-gray-500">
+                  size: {product.size} | color: {product.color}
+                </p>
+
+                {/* quantity add and remove */}
+                <div className="flex items-center mt-2">
+                  <button className="px-2 py-1 text-xl font-medium rounded border">
+                    -
+                  </button>
+                  <span className="mx-4">{product.quantity}</span>
+                  <button className="px-2 py-1 text-xl font-medium rounded border">
+                    +
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         ))}
