975a19c8f64c83cd81d517258c77384ad57b70b3
陶浩南 <taoaaron5@gmail.com>
2025-04-25 19:26:06 +1000

    the footer creation

---
+++ frontend/src/components/Common/Footer.jsx
@@ -0,0 +1,13 @@
+/*
+ * @Date: 2025-04-25 19:25:13
+ * @LastEditors: 陶浩南 taoaaron5@gmail.com
+ * @LastEditTime: 2025-04-25 19:25:17
+ * @FilePath: /E-Commerce-Rabbit/frontend/src/components/Common/Footer.jsx
+ */
+import React from "react";
+
+const Footer = () => {
+  return <div>Footer</div>;
+};
+
+export default Footer;
--- frontend/src/components/Layout/UserLayout.jsx
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -1,11 +1,12 @@
 /*
  * @Date: 2025-04-20 21:54:41
  * @LastEditors: 陶浩南 taoaaron5@gmail.com
- * @LastEditTime: 2025-04-21 20:10:08
- * @FilePath: /E-Commerce-Rabbit/frontend/src/components/Layout/Userlayout.jsx
+ * @LastEditTime: 2025-04-25 19:25:56
+ * @FilePath: /E-Commerce-Rabbit/frontend/src/components/Layout/UserLayout.jsx
  */
 import React from "react";
 import Header from "../Common/Header";
+import Footer from "../Common/Footer";
 const Userlayout = () => {
   return (
     <>
@@ -13,6 +14,7 @@ const Userlayout = () => {
       <Header />
       {/* main content */}
       {/* footer */}
+      <Footer />
     </>
   );
 };
