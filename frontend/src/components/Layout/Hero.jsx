0afb0e44346763d5b46f6bcd84fb03acac801d2c
陶浩南 <taoaaron5@gmail.com>
2025-04-26 18:35:46 +1000

    main contents creation

---
+++ frontend/src/components/Layout/Hero.jsx
@@ -0,0 +1,13 @@
+/*
+ * @Date: 2025-04-26 18:35:16
+ * @LastEditors: 陶浩南 taoaaron5@gmail.com
+ * @LastEditTime: 2025-04-26 18:35:20
+ * @FilePath: /E-Commerce-Rabbit/frontend/src/components/Layout/Hero.jsx
+ */
+import React from "react";
+
+const Hero = () => {
+  return <div></div>;
+};
+
+export default Hero;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+/*
+ * @Date: 2025-04-26 18:34:57
+ * @LastEditors: 陶浩南 taoaaron5@gmail.com
+ * @LastEditTime: 2025-04-26 18:35:00
+ * @FilePath: /E-Commerce-Rabbit/frontend/src/pages/Home.jsx
+ */
+import React from "react";
+import Hero from "../components/Layout/Hero";
+
+const Home = () => {
+  return (
+    <>
+      <div>
+        <Hero />
+      </div>
+      ;
+    </>
+  );
+};
+
+export default Home;
