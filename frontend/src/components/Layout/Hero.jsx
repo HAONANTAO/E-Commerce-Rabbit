77c5dcf89412e90041c2c51ecad77b5333682eca
陶浩南 <taoaaron5@gmail.com>
2025-04-26 19:05:42 +1000

    hero section text

--- frontend/src/components/Layout/Hero.jsx
+++ frontend/src/components/Layout/Hero.jsx
@@ -6,6 +6,7 @@
  */
 import React from "react";
 import heroImage from "../../assets/rabbit-hero.webp";
+import { Link } from "react-router-dom";
 const Hero = () => {
   return (
     <>
@@ -22,8 +23,17 @@ const Hero = () => {
         <div className="flex absolute inset-0 justify-center items-center bg-black bg-opacity-10">
           <div className="p-6 text-center text-white">
             <h1 className="mb-4 text-4xl font-bold tracking-tighter uppercase md:text-9xl">
-              Vacation
+              Vacation <br /> Ready
             </h1>
+            <p className="mb-6 text-sm tracking-tighter md:text-lg">
+              Explore our wide range of products.
+            </p>
+            {/* TODO:Link jump */}
+            <Link
+              to="#"
+              className="px-6 py-2 text-lg bg-white rounded-sm text-gray-950">
+              Shop Now
+            </Link>
           </div>
         </div>
       </section>
