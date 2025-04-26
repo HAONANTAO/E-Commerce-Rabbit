4266c022042088c8d90d2cffa7b780842a2221b7
陶浩南 <taoaaron5@gmail.com>
2025-04-26 19:07:09 +1000

    GenderCollectionSection creation

---
+++ frontend/src/components/Products/GenderCollectionSection.jsx
@@ -0,0 +1,13 @@
+/*
+ * @Date: 2025-04-26 19:06:23
+ * @LastEditors: 陶浩南 taoaaron5@gmail.com
+ * @LastEditTime: 2025-04-26 19:06:39
+ * @FilePath: /E-Commerce-Rabbit/frontend/src/components/Products/GenderCollectionSection.jsx
+ */
+import React from "react";
+
+const GenderCollectionSection = () => {
+  return <div></div>;
+};
+
+export default GenderCollectionSection;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,18 @@
 /*
  * @Date: 2025-04-26 18:34:57
  * @LastEditors: 陶浩南 taoaaron5@gmail.com
- * @LastEditTime: 2025-04-26 18:55:51
+ * @LastEditTime: 2025-04-26 19:06:59
  * @FilePath: /E-Commerce-Rabbit/frontend/src/pages/Home.jsx
  */
 import React from "react";
 import Hero from "../components/Layout/Hero";
-
+import GenderCollectionSection from "../components/Products/GenderCollectionSection";
 const Home = () => {
   return (
     <>
       <div>
         <Hero />
+        <GenderCollectionSection />
       </div>
     </>
   );
