ee17637d7c84bfdea578396921e98daaa1d70fb1
陶浩南 <taoaaron5@gmail.com>
2025-04-27 21:12:14 +1000

    setSelectSize

--- frontend/src/components/Products/ProductDetails.jsx
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -1,7 +1,7 @@
 /*
  * @Date: 2025-04-27 11:33:34
  * @LastEditors: 陶浩南 taoaaron5@gmail.com
- * @LastEditTime: 2025-04-27 21:09:40
+ * @LastEditTime: 2025-04-27 21:12:03
  * @FilePath: /E-Commerce-Rabbit/frontend/src/components/Products/ProductDetails.jsx
  */
 import React, { useEffect, useState } from "react";
@@ -111,7 +111,12 @@ const ProductDetails = () => {
                 <p className="text-gray-700">Size:</p>
                 <div className="flex gap-2 mt-2">
                   {selectedProducts.sizes.map((size, index) => (
-                    <button key={index} className="px-4 py-2 rounded border">
+                    <button
+                      onClick={() => setSelectSize(size)}
+                      key={index}
+                      className={`${
+                        selectSize === size ? "bg-black text-white" : ""
+                      } px-4 py-2 rounded border`}>
                       {size}
                     </button>
                   ))}
