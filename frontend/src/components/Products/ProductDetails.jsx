9467eadea02b36a084ca86d4c76fc39a263aed51
陶浩南 <taoaaron5@gmail.com>
2025-04-27 11:34:01 +1000

    ProductDetails creation

---
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -0,0 +1,17 @@
+/*
+ * @Date: 2025-04-27 11:33:34
+ * @LastEditors: 陶浩南 taoaaron5@gmail.com
+ * @LastEditTime: 2025-04-27 11:33:37
+ * @FilePath: /E-Commerce-Rabbit/frontend/src/components/Products/ProductDetails.jsx
+ */
+import React from "react";
+
+const ProductDetails = () => {
+  return (
+    <>
+      <div>ProductDetails</div>;
+    </>
+  );
+};
+
+export default ProductDetails;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,6 +8,7 @@ import React from "react";
 import Hero from "../components/Layout/Hero";
 import GenderCollectionSection from "../components/Products/GenderCollectionSection";
 import NewArrivals from "../components/Products/NewArrivals";
+import ProductDetails from "../components/Products/ProductDetails";
 const Home = () => {
   return (
     <>
@@ -15,6 +16,11 @@ const Home = () => {
         <Hero />
         <GenderCollectionSection />
         <NewArrivals />
+        {/* best seller */}
+        <div>
+          <h2 className="mb-4 text-3xl font-bold text-center">Best Seller</h2>
+          <ProductDetails />
+        </div>
       </div>
     </>
   );
