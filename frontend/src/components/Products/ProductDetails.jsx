07d41b21eeb22b98da843bbe936272146073817b
陶浩南 <taoaaron5@gmail.com>
2025-04-27 20:39:41 +1000

    prices,sizes,colors

--- frontend/src/components/Products/ProductDetails.jsx
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -1,7 +1,7 @@
 /*
  * @Date: 2025-04-27 11:33:34
  * @LastEditors: 陶浩南 taoaaron5@gmail.com
- * @LastEditTime: 2025-04-27 20:16:51
+ * @LastEditTime: 2025-04-27 20:39:33
  * @FilePath: /E-Commerce-Rabbit/frontend/src/components/Products/ProductDetails.jsx
  */
 import React from "react";
@@ -49,6 +49,62 @@ const ProductDetails = () => {
                   />
                 </div>
               ))}
+            </div>
+
+            {/* right section */}
+            <div className="flex flex-col md:w-1/2 md:ml-10">
+              <h1 className="mb-2 text-2xl font-semibold md:text-3xl">
+                {selectedProducts.name}
+              </h1>
+
+              {/* prices */}
+              {/* line-through会在文本上添加一条删除线。原价 */}
+              <p className="mb-1 text-lg text-gray-600 line-through">
+                {selectedProducts.originalPrice &&
+                  `${selectedProducts.originalPrice}`}
+              </p>
+              <p className="mb-2 text-xl text-gray-500">
+                ${selectedProducts.price}
+              </p>
+              {/* description */}
+              <p className="mb-4 text-gray-600">
+                {selectedProducts.description}
+              </p>
+
+              {/* colors */}
+              <div className="mb-4">
+                <p className="text-gray-700">Color:</p>
+                <div className="flex gap-2 mt-2">
+                  {selectedProducts.colors.map((col, index) => (
+                    <div>
+                      <button
+                        key={index}
+                        className={` w-8 h-8 rounded-full border`}
+                        // 因为背景颜色是动态的
+                        style={{
+                          backgroundColor: col.toLowerCase(),
+                          filter: "brightness(0.7)",
+                        }}></button>
+                    </div>
+                  ))}
+                </div>
+              </div>
+
+              {/* sizes */}
+              <div className="mb-4">
+                <p className="text-gray-700">Size:</p>
+                <div className="flex gap-2 mt-2">
+                  {selectedProducts.sizes.map((size, index) => (
+                    <div>
+                      <button key={index} className="px-4 py-2 rounded border">
+                        {size}
+                      </button>
+                    </div>
+                  ))}
+                </div>
+              </div>
+
+              
             </div>
           </div>
         </div>
