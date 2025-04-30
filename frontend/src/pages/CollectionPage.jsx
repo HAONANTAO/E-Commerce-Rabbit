d08ad400e47ef9a79c62b7f234676c0349d4a436
陶浩南 <taoaaron5@gmail.com>
2025-04-30 20:15:28 +1000

    handleClickOutside function logic

--- frontend/src/pages/CollectionPage.jsx
+++ frontend/src/pages/CollectionPage.jsx
@@ -1,15 +1,33 @@
 /*
  * @Date: 2025-04-29 22:17:20
  * @LastEditors: 陶浩南 taoaaron5@gmail.com
- * @LastEditTime: 2025-04-30 20:07:08
+ * @LastEditTime: 2025-04-30 20:14:43
  * @FilePath: /E-Commerce-Rabbit/frontend/src/pages/CollectionPage.jsx
  */
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { topWearsWomen } from "@/utils/mockdb.js";
 import { FaFilter } from "react-icons/fa";
+import FilterSidebar from "@/components/Products/FilterSidebar";
 const CollectionPage = () => {
   const [products, setProducts] = useState([]);
+  const SidebarRef = useRef(null);
+  // 点按钮打开 点击其他关闭
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+  // 点击外面关闭sidebar
+  const handleClickOutside = (e) => {
+    // 事件是否发生在 Sidebar 组件外
+    if (SidebarRef.current && !SidebarRef.current.contains(e.target)) {
+      setIsSidebarOpen(false);
+    }
+  };
+  useEffect(() => {
+    // add event listener for clicks
+    document.addEventListener("mousedown", handleClickOutside);
+  }, []);
   useEffect(() => {
     // 模拟后端拿数据
     setTimeout(() => {
@@ -22,12 +40,12 @@ const CollectionPage = () => {
       <button
         type="button"
         className="flex justify-center items-center px-2 border lg:hidden">
-        <FaFilter className="mr-2"/>
+        <FaFilter className="mr-2" />
       </button>
 
       {/* filter sidebar */}
-      <div>
-        
+      <div ref={SidebarRef}>
+        <FilterSidebar />
       </div>
     </div>
   );
