72bce852dafff812198bef81d282fa9053360660
陶浩南 <taoaaron5@gmail.com>
2025-05-04 20:41:33 +1000

    test create product good

---
+++ backend/routes/productRoute.js
@@ -0,0 +1,65 @@
+import express from "express";
+import Product from "../models/Product.js";
+import protect from "../middleware/AuthMiddleware.js";
+
+const productRouter = express.Router();
+
+// @routes POST /api/
+// @desc Create new product in database
+// @access Private/Admin
+productRouter.post("/", protect, async (req, res) => {
+  try {
+    // 拿到产品的数据
+    const {
+      name,
+      description,
+      price,
+      discountPrice,
+      countInStock,
+      category,
+      brand,
+      sizes,
+      colors,
+      collections,
+      material,
+      gender,
+      images,
+      isFeatured,
+      isPublished,
+      tags,
+      dimensions,
+      weight,
+      sku,
+    } = req.body;
+    const product = new Product({
+      name,
+      description,
+      price,
+      discountPrice,
+      countInStock,
+      category,
+      brand,
+      sizes,
+      colors,
+      collections,
+      material,
+      gender,
+      images,
+      isFeatured,
+      isPublished,
+      tags,
+      dimensions,
+      weight,
+      sku,
+      user: req.user._id, //refer to current  admin user
+    });
+
+    const createdProduct = await product.save();
+    res.status(201).json(createdProduct);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server Error");
+  }
+});
+
+export default productRouter;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2025-05-04 17:25:45
  * @LastEditors: 陶浩南 taoaaron5@gmail.com
- * @LastEditTime: 2025-05-04 18:00:00
+ * @LastEditTime: 2025-05-04 20:28:02
  * @FilePath: /E-Commerce-Rabbit/backend/server.js
  */
 import express from "express";
@@ -9,6 +9,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoute.js";
+import productRoutes from "./routes/productRoute.js";
 // 初始化
 const app = express();
 // 中间件 允许跨域 json解析
@@ -23,8 +24,10 @@ connectDB();
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
 // API Routes
 app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
