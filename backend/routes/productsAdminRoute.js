29214dbb747b9b3e359b8649e3c7f12bfcd4b1db
陶浩南 <taoaaron5@gmail.com>
2025-05-08 20:55:23 +1000

    productsAdminRoute

---
+++ backend/routes/productsAdminRoute.js
@@ -0,0 +1,27 @@
+/*
+ * @Date: 2025-05-08 20:49:13
+ * @LastEditors: 陶浩南 taoaaron5@gmail.com
+ * @LastEditTime: 2025-05-08 20:54:40
+ * @FilePath: /E-Commerce-Rabbit/backend/routes/productsAdminRoute.js
+ */
+import express from "express";
+import { protect, admin } from "../middleware/AuthMiddleware.js";
+import Product from "../models/Product.js";
+
+const productsAdminRoute = express.Router();
+
+//@ routes GET /api/admin/products
+//@desc Get all products(only for admin)
+//@access Private/Admin
+productsAdminRoute.get("/", protect, admin, async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    let products = await Product.find({});
+    res.status(200).json(products);
+    console.log(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+export default productsAdminRoute;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2025-05-04 17:25:45
  * @LastEditors: 陶浩南 taoaaron5@gmail.com
- * @LastEditTime: 2025-05-08 20:28:11
+ * @LastEditTime: 2025-05-08 20:52:38
  * @FilePath: /E-Commerce-Rabbit/backend/server.js
  */
 import express from "express";
@@ -16,6 +16,7 @@ import orderRouter from "./routes/orderRoute.js";
 import uploadRouter from "./routes/uploadRoute.js";
 import subscribersRouter from "./routes/subscriberRoute.js";
 import adminRouter from "./routes/adminRoute.js";
+import productsAdminRouter from "./routes/productsAdminRoute.js";
 // 初始化
 const app = express();
 // 中间件 允许跨域 json解析
@@ -40,6 +41,7 @@ app.use("/api/orders", orderRouter);
 app.use("/api/upload", uploadRouter);
 app.use("/api/subscribe", subscribersRouter);
 app.use("/api/admin", adminRouter);
+app.use("/api/admin/products", productsAdminRouter);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
