eb6a56ea3d032c55c5c11b8cb9e01fee59b64b9b
陶浩南 <taoaaron5@gmail.com>
2025-05-10 16:22:35 +1000

    orderSlice.reducer

---
+++ frontend/src/redux/slices/orderSlice.js
@@ -0,0 +1,93 @@
+/*
+ * @Date: 2025-05-10 16:13:04
+ * @LastEditors: 陶浩南 taoaaron5@gmail.com
+ * @LastEditTime: 2025-05-10 16:21:38
+ * @FilePath: /E-Commerce-Rabbit/frontend/src/redux/slices/orderSlice.js
+ */
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+//async chunk to fetch orders of user
+export const fetchOrders = createAsyncThunk(
+  "orders/fetchOrders",
+  //_意思就是没有
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_BACKEND.URL}/api/orders/my-orders}`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+          },
+        },
+      );
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      return rejectWithValue(error.response.data);
+    }
+  },
+);
+
+//async chunk to fetch order details by id
+export const fetchOrderDetails = createAsyncThunk(
+  "orders/fetchOrderDetails",
+  async (orderId, { rejectWithValue }) => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_BACKEND.URL}/api/orders/${orderId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+          },
+        },
+      );
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      return rejectWithValue(error.response.data);
+    }
+  },
+);
+
+const orderSlice = createSlice({
+  name: "orders",
+  initialState: {
+    orders: [],
+    totalOrders: 0,
+    orderDetails: null,
+    loading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      // fetch orders
+      .addCase(fetchOrders.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchOrders.fulfilled, (state, action) => {
+        state.loading = false;
+        state.orders = action.payload;
+      })
+      .addCase(fetchOrders.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload.message;
+      })
+      // fetchOrderDetails
+      .addCase(fetchOrderDetails.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchOrderDetails.fulfilled, (state, action) => {
+        state.loading = false;
+        state.orderDetails = action.payload;
+      })
+      .addCase(fetchOrderDetails.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload.message;
+      });
+  },
+});
+export default orderSlice.reducer;
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2025-05-10 14:00:06
  * @LastEditors: 陶浩南 taoaaron5@gmail.com
- * @LastEditTime: 2025-05-10 15:25:24
+ * @LastEditTime: 2025-05-10 16:12:40
  * @FilePath: /E-Commerce-Rabbit/frontend/src/redux/store.js
  */
 
@@ -9,12 +9,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import productReducer from "./slices/productSlice";
 import cartReducer from "./slices/cartSlice";
+import checkoutReducer from "./slices/checkoutSlice";
 // 创建store
 const store = configureStore({
   reducer: {
     auth: authReducer,
     products: productReducer,
     cart: cartReducer,
+    checkout: checkoutReducer,
   },
 });
 export default store;
