2aadc827b2b4f36315866d659b525490128f742f
陶浩南 <taoaaron5@gmail.com>
2025-05-10 14:00:56 +1000

    redux store creation

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,14 @@
+/*
+ * @Date: 2025-05-10 14:00:06
+ * @LastEditors: 陶浩南 taoaaron5@gmail.com
+ * @LastEditTime: 2025-05-10 14:00:12
+ * @FilePath: /E-Commerce-Rabbit/frontend/src/redux/store.js
+ */
+
+import { configureStore } from "@reduxjs/toolkit";
+
+// 创建store
+const store = configureStore({
+  reducer: {},
+});
+export default store;
