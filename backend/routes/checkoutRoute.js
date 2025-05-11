919c861eb2c38a0447c69a377560ea0292bdecb8
陶浩南 <taoaaron5@gmail.com>
2025-05-11 17:28:41 +1000

    Update checkoutRoute.js

--- backend/routes/checkoutRoute.js
+++ backend/routes/checkoutRoute.js
@@ -2,7 +2,7 @@ import express from "express";
 import Order from "../models/Order.js";
 import Checkout from "../models/Checkout.js";
 import Cart from "../models/Cart.js";
-import Product from "../models/Product.js";
+// import Product from "../models/Product.js";
 import { protect } from "../middleware/AuthMiddleware.js";
 const CheckOutRoute = express.Router();
 
