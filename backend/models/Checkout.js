38761a0c2a34ddc8a2bd1ba5422c75ef5e868148
陶浩南 <taoaaron5@gmail.com>
2025-05-11 17:53:39 +1000

    deployment issue4

---
+++ backend/models/Checkout.js
@@ -0,0 +1,97 @@
+/*
+ * @Date: 2025-05-07 20:11:09
+ * @LastEditors: 陶浩南 taoaaron5@gmail.com
+ * @LastEditTime: 2025-05-11 17:53:36
+ * @FilePath: /E-Commerce-Rabbit/backend/models/checkout.js
+ */
+import mongoose from "mongoose";
+
+const CheckoutItemSchema = new mongoose.Schema(
+  {
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    size: String,
+    color: String,
+  },
+  { _id: false },
+);
+
+const CheckoutSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    checkoutItems: [CheckoutItemSchema],
+    shippingAddress: {
+      address: {
+        type: String,
+        required: true,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+      postalCode: {
+        type: String,
+        required: true,
+      },
+      country: {
+        type: String,
+        required: true,
+      },
+    },
+    paymentMethod: {
+      type: String,
+      required: true,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+    isPaid: {
+      type: Boolean,
+      default: false,
+    },
+    paidAt: {
+      type: Date,
+    },
+    paymentStatus: {
+      type: String,
+      default: "Pending",
+    },
+    paymentDetails: {
+      // - 可以存储任意类型的值 不进行类型检查
+      type: mongoose.Schema.Types.Mixed, //store payment-related details(transactionID,paypal)
+    },
+    isFinalized: {
+      type: Boolean,
+      default: false,
+    },
+    finalizedAt: {
+      type: Date,
+    },
+  },
+  { timestamps: true },
+);
+export default mongoose.model("Checkout", CheckoutSchema);
